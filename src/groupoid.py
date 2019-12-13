6051547b7c2089162de2b0a8c94eca809617cd97
Paul Cappaert <paul.cappaert@gmail.com>
2019-12-13 16:22:46 -0500

    added groupoid object and tests

---
+++ src/groupoid.py
@@ -0,0 +1,36 @@
+from itertools import product
+
+class Groupoid:
+
+    def __init__(self, elements, comm=True, zero=0):
+        self.elements = tuple(elements + (zero,))
+        self.comm = comm
+
+        if self.comm:
+            self.mult_table = {frozenset({a, b}): zero for (a, b) in product(self.elements, self.elements)}
+        else:
+            self.mult_table = {(a, b): zero for (a, b) in product(self.elements, self.elements)}
+
+    def get(self, a, b):
+        if self.comm:
+            return self.mult_table[frozenset({a, b})]
+        else:
+            return self.mult_table[(a, b)]
+
+    def set(self, a, b, prod):
+        if self.comm:
+            self.mult_table[frozenset({a, b})] = prod
+        else:
+            self.mult_table[(a, b)] = prod
+
+
+
+def is_assoc(groupoid):
+    for a in groupoid.elements:
+        for b in groupoid.elements:
+            for c in groupoid.elements:
+                p1 = groupoid.get(a, groupoid.get(b, c))
+                p2 = groupoid.get(groupoid.get(a, b), c)
+                if not p1 == p2:
+                    return False
+    return True
---
+++ src/tests/test_groupoid.py
@@ -0,0 +1,54 @@
+import unittest
+from src.groupoid import Groupoid, is_assoc
+
+class TestGroupoid(unittest.TestCase):
+
+    def setUp(self):
+        self.grpd = Groupoid((1, 2, 3), comm=False)
+        self.grpd_assoc = Groupoid((1, 2, 3))
+
+    def test_prod(self):
+        self.assertEqual(self.grpd.get(1, 2), 0)
+        self.grpd.set(1, 2, 3)
+        self.assertEqual(self.grpd.get(1, 2), 3)
+        self.assertEqual(self.grpd.get(2, 1), 0)
+
+    def test_comm_prod(self):
+        self.assertEqual(self.grpd_assoc.get(1, 2), 0)
+        self.grpd_assoc.set(1, 2, 3)
+        self.assertEqual(self.grpd_assoc.get(1, 2), 3)
+        self.assertEqual(self.grpd_assoc.get(2, 1), 3)
+
+    def test_square(self):
+        self.assertEqual(self.grpd.get(1, 1), 0)
+        self.grpd.set(1, 1, 2)
+        self.assertEqual(self.grpd.get(1, 1), 2)
+
+    def test_comm_square(self):
+        self.assertEqual(self.grpd.get(1, 1), 0)
+        self.grpd.set(1, 1, 2)
+        self.assertEqual(self.grpd.get(1, 1), 2)
+
+class TestIsAssoc(unittest.TestCase):
+
+    def setUp(self):
+        self.grpd = Groupoid((1, 2, 3), comm=False)
+        self.grpd_comm = Groupoid((1, 2, 3))
+
+    def test_assoc(self):
+        self.assertTrue(is_assoc(self.grpd))
+        self.assertTrue(is_assoc(self.grpd_comm))
+
+    def test_assoc_comm(self):
+        self.grpd_comm.set(1, 2, 3)
+        self.grpd_comm.set(2, 3, 3)
+        self.grpd_comm.set(1, 3, 3)
+        self.grpd_comm.set(3, 3, 3)
+        self.grpd_comm.set(1, 1, 1)
+        self.grpd_comm.set(2, 2, 2)
+        self.assertTrue(is_assoc(self.grpd_comm))
+
+    def test_noassoc_comm(self):
+        self.grpd_comm.set(1, 2, 3)
+        self.grpd_comm.set(2, 3, 3)
+        self.assertFalse(is_assoc(self.grpd_comm))
