2eac73022a27438c0637ad8068d60c7d65691143
Paul Cappaert <paul.cappaert@gmail.com>
2020-01-04 12:43:01 -0500

    added basic graph functions

---
+++ src/graph.py
@@ -0,0 +1,46 @@
+import itertools
+
+'''
+gets a simple graph with the given edges
+returns a dictionary from vertices to their neighborhoods
+'''
+def graph_from_edges(*args):
+    graph = {}
+    for e in args:
+        for i in range(2):
+            if e[i] in graph:
+                graph[e[i]].add(e[1 - i])
+            else:
+                graph[e[i]] = {e[1 - i]}
+    return graph
+
+def _poss_vertex(nhood, graph):
+    poss = set()
+    for x in graph:
+        if (graph[x].union({x})).issuperset(nhood):
+            poss.add(x)
+    not_empty = len(poss)
+    return not_empty, poss
+
+'''
+gets the possible products of zero divisors with the given zero element
+return a dictionary from products to possible values
+if insufficient zdg, returns false and possible products
+'''
+def poss_mappings(graph, zero=0):
+    poss = True
+    maps = {}
+
+    for a, b in itertools.combinations(graph, 2):
+        if a not in graph[b]:
+            nhood = graph[a].union(graph[b])
+            not_empty, maps[frozenset({a, b})] = _poss_vertex(nhood, graph)
+            poss = not_empty and poss
+
+    for a in graph.keys():
+        nhood = graph[a]
+        not_empty, maps[frozenset({a})] = _poss_vertex(nhood, graph)
+        maps[frozenset({a})].add(zero)
+        poss = not_empty and poss
+
+    return poss, maps
---
+++ src/tests/test_graph.py
@@ -0,0 +1,60 @@
+import unittest
+from src.graph import graph_from_edges, poss_mappings
+
+class TestGraphFromEdges(unittest.TestCase):
+
+    def test_neighborhood(self):
+        G = graph_from_edges((1, 2))
+        self.assertEqual(G[1], {2})
+        self.assertEqual(G[2], {1})
+
+    def test_neighborhood1(self):
+        G = graph_from_edges((1, 2), (2, 3))
+        self.assertEqual(G[1], {2})
+        self.assertEqual(G[2], {1, 3})
+        self.assertEqual(G[3], {2})
+
+    def test_neighborhood2(self):
+        G = graph_from_edges((1, 2), (2, 3), (3, 4), (4, 1))
+        self.assertEqual(G[1], {2, 4})
+        self.assertEqual(G[2], {1, 3})
+        self.assertEqual(G[3], {2, 4})
+        self.assertEqual(G[4], {1, 3})
+
+    def test_neighborhood3(self):
+        G = graph_from_edges((1, 2), (1, 3), (1, 4))
+        self.assertEqual(G[1], {2, 3, 4})
+        self.assertEqual(G[2], {1})
+        self.assertEqual(G[3], {1})
+        self.assertEqual(G[4], {1})
+
+class TestPossMappings(unittest.TestCase):
+
+    def test_poss1(self):
+        poss, maps = poss_mappings(graph_from_edges((1, 2), (2, 3)))
+        self.assertTrue(poss)
+        self.assertEqual(maps[frozenset({1, 3})], {1, 2, 3})
+        self.assertEqual(maps[frozenset({1})], {1, 2, 3, 0})
+        self.assertEqual(maps[frozenset({2})], {2, 0})
+        self.assertEqual(maps[frozenset({3})], {1, 2, 3, 0})
+
+    def test_poss2(self):
+        poss, maps = poss_mappings(graph_from_edges(
+        (1, 2), (2, 3), (3, 4)))
+        self.assertTrue(poss)
+        self.assertEqual(maps[frozenset({1, 4})], {2, 3})
+        self.assertEqual(maps[frozenset({2, 4})], {2})
+        self.assertEqual(maps[frozenset({1, 3})], {3})
+        self.assertEqual(maps[frozenset({1})], {1, 2, 3, 0})
+        self.assertEqual(maps[frozenset({2})], {2, 0})
+        self.assertEqual(maps[frozenset({3})], {3, 0})
+        self.assertEqual(maps[frozenset({4})], {4, 3, 2, 0})
+
+
+    def test_impossible(self):
+        poss, maps = poss_mappings(graph_from_edges((1, 2), (3, 4)))
+        self.assertFalse(poss)
+        self.assertEqual(maps[frozenset({1, 3})], set())
+        self.assertEqual(maps[frozenset({2, 4})], set())
+        self.assertEqual(maps[frozenset({1, 4})], set())
+        self.assertEqual(maps[frozenset({2, 3})], set())
